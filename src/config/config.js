794652f7b15404099819722becb730f6c9950b08
EmmanuelGutierrez <emma.g2705@gmail.com>
2021-10-08 22:28:24 -0300

    Implemente dotenv

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,16 @@
-const config = require('./env.json')
+require('dotenv').config();
 
-const EMAIL = config.EMAIL
-    , EMAIL_PASSWORD = config.EMAIL_PASSWORD
-    , DB_LOCAL = config.DB_LOCAL
+const nodeEnv = (process.env.ENV = process.env.ENV || 'dev').trim();
+
+const ENV=nodeEnv 
+    ,PORT=process.env.PORT
+    ,EMAIL = process.env.EMAIL
+    ,EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
+    ,DB_LOCAL = process.env.DB_LOCAL
 
 module.exports = {
+    ENV,
+    PORT,
     EMAIL,
     EMAIL_PASSWORD,
     DB_LOCAL
