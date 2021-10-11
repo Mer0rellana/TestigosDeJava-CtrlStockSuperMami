278990db95895226fec2a1df8c8b4dad3d5708c6
1w1111789AimonettiJoaquin <Yaimonetti@hotmail.com>
2021-10-10 23:15:44 -0300

    putItem bien perron wachos, me costo una banda

---
+++ src/apis/item/controllers/putItemState.js
@@ -0,0 +1,24 @@
+const { ItemSchema: Item } = require("../../../models/item");
+const ErrorModel = require("../../../models/api-error");
+const yup = require("yup");
+const Moment = require("moment");
+
+const ItemUpdateState = async (req, res) => {
+  try {
+    const token = res.locals.payload;
+    if (token.role === "Admin") {
+      const fecha = Moment.now();
+      const { id } = req.params;
+      await Item.updateOne({ _id: id }, { $set: { state: "inactivo",  updateDate: fecha} });
+      return res.status(200).send({ message: "Un Exito" });
+    } else {
+      return new ErrorModel()
+        .newUnauthorized("Usuario no autorizado")
+        .send(res);
+    }
+  } catch (err) {
+    return new ErrorModel().newInternalServerError(err.message).send(res);
+  }
+};
+
+module.exports = ItemUpdateState;
