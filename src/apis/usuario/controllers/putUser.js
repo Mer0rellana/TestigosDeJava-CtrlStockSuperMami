d17a3ccdb46dd090564d7394df349f3e104a714d
Mercedes <mer.p.orellana@gmail.com>
2021-10-11 12:08:30 -0300

    user update

---
+++ src/apis/usuario/controllers/putUser.js
@@ -0,0 +1,39 @@
+const  Validator  = require('../../../utils/validator');
+const yup = require("yup");
+const ErrorModel = require('../../../models/api-error');
+
+const schema = yup.object().shape({
+    name: yup.string(),
+    tel: yup.number(),
+    mail: yup.string().email().transform((dato) => dato.toLowerCase())
+})
+
+const UpdateUser = async (req, res) =>{
+    try { 
+
+        const request = await Validator(req.body, schema);
+        if (request.err) return new ErrorModel().newBadRequest(request.data).send(res);
+
+        const id = token.id;
+
+        const user = new User({
+            ...req.body,
+            UpdatedAt: moment.now(), 
+        });
+
+        const err = user.validateSync();
+        if (err) return new ErrorModel().newBadRequest(err.message).send(res);
+
+        const doc = await user.findOneAndUpdate(id, user);
+
+        const sending = await SendTemplate(user.mail, "Control Stock Super Mami - Perfil actualizado", "sendEmail", { principalInfo: `El usuario ${user.id} ha actualizado su perfil con exito` });
+        if (sending.error) return new ErrorModel(535, sending.error, "Error en el envío de email").send(res);
+
+        return res.status(200).send(doc);
+
+    } catch (err) {
+        return new ErrorModel().newInternalServerError(err.message).send(res);
+    }
+}
+
+module.exports = UpdateUser;
--- src/apis/usuario/index.js
+++ src/apis/usuario/index.js
@@ -6,6 +6,7 @@ const MyProfile = require('./controllers/getMyProfile');
 const LoginUser = require('./controllers/postLogin');
 const CreateUser = require('./controllers/postNewUser');
 const RecoverPassword = require('./controllers/postRecoverPassword');
+const UpdateUser = require('./controllers/putUser');
 const router = express.Router();
 
 router.get('/ejemplo', ensureUserAuthenticated, Ejemplo); //ejemplo para sacar datos del token
@@ -14,5 +15,6 @@ router.post('/add', CreateUser);
 router.post('/recoverPassword', RecoverPassword);
 router.get('/myProfile', ensureUserAuthenticated, MyProfile);
 router.get('/', ensureUserAuthenticated, GetUsers);
+router.put('/update-user', ensureUserAuthenticated ,UpdateUser);
 
 module.exports = router;
