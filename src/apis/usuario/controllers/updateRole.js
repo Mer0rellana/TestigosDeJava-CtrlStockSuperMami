3e08c6cea9587de0c3badaf72a20276afcf4dec9
Mercedes <mer.p.orellana@gmail.com>
2021-10-11 20:13:24 -0300

    user update role - change state

---
+++ src/apis/usuario/controllers/updateRole.js
@@ -0,0 +1,40 @@
+const Validator = require('../../../utils/validator');
+const yup = require("yup");
+const ErrorModel = require('../../../models/api-error');
+const User = require('../../../models/user');
+const moment = require('moment');
+const SendTemplate = require('../../../utils/sendMail');
+
+const schema = yup.object().shape({
+    role: yup.string()
+})
+
+const UpdateRole = async (req, res) => {
+    try {
+
+        const token = res.locals.payload;
+        if (token.role === "Admin") {
+
+            const request = await Validator(req.body, schema);
+            if (request.err) return new ErrorModel().newBadRequest(request.data).send(res);
+
+            const { id } = req.params;
+            await User.updateOne({
+                id: id
+            }, {
+                role: request.data.role,
+                updatedAt: moment.now()
+            });
+
+            return res.status(200).send({ message: "Perfil actualizado con éxito" });
+
+        } else {
+            return new ErrorModel().newUnauthorized().send(res);
+        }
+
+    } catch (err) {
+        return new ErrorModel().newInternalServerError(err.message).send(res);
+    }
+}
+
+module.exports = UpdateRole;
--- src/apis/usuario/index.js
+++ src/apis/usuario/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { ensureUserAuthenticated } = require('../../utils/token');
-const  Ejemplo  = require('./controllers/ejemplo');
+const Ejemplo  = require('./controllers/ejemplo');
 const GetUsers = require('./controllers/getUsers');
 const MyProfile = require('./controllers/getMyProfile');
 const LoginUser = require('./controllers/postLogin');
 const CreateUser = require('./controllers/postNewUser');
 const RecoverPassword = require('./controllers/postRecoverPassword');
+const DeleteUser = require('./controllers/deleteUser');
+const UpdateRole = require('./controllers/updateRole');
 const router = express.Router();
 
 router.get('/ejemplo', ensureUserAuthenticated, Ejemplo); //ejemplo para sacar datos del token
@@ -14,5 +16,7 @@ router.post('/add', ensureUserAuthenticated ,CreateUser);
 router.post('/recoverPassword', RecoverPassword);
 router.get('/myProfile', ensureUserAuthenticated, MyProfile);
 router.get('/', ensureUserAuthenticated, GetUsers);
+router.put('/deleteUser/:id', ensureUserAuthenticated, DeleteUser);
+router.put('/changeRole/:id', ensureUserAuthenticated, UpdateRole);
 
 module.exports = router;
