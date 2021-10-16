de23011f3e69922ecca01ed9f5ba2042d2027050
1w1111789AimonettiJoaquin <Yaimonetti@hotmail.com>
2021-10-16 13:27:20 -0300

    deposito por almacenamiento

--- src/apis/storage/controllers/putStorage.js
+++ src/apis/storage/controllers/putStorage.js
@@ -1,7 +1,7 @@
 const StorageSchema = require("../../../models/storage");
 const ErrorModel = require("../../../models/api-error");
 const Moment = require("moment");
-
+//storageupdate
 const StorageUpdateState = async (req,res) => {
     try{
         const token = res.locals.payload;
@@ -9,8 +9,8 @@ const StorageUpdateState = async (req,res) => {
             const fecha = Moment.now();
             const { id } = req.params;
             const storage = await StorageSchema.updateOne({id : id}, { state:"Inactivo", updatedAt: fecha});
-            if (storage.matchedCount === 0) return new ErrorModel().newNotFound("El almacenamiento no existe").send(res);
-            return res.status(200).send({message: "El almacenamiento fue dado de bajo de forma exitosa"});
+            if (storage.matchedCount === 0) return new ErrorModel().newNotFound("El deposito no existe").send(res);
+            return res.status(200).send({message: "El deposito fue dado de baja de forma exitosa"});
         }
         else{
             return new ErrorModel().newUnauthorized("Usuario no autorizado").send(res);
