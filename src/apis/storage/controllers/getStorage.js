229388aea54f618ca3fe7412ee8d0f78659e20e2
1w1111789AimonettiJoaquin <Yaimonetti@hotmail.com>
2021-10-16 20:04:41 -0300

    porcentaje area

--- src/apis/storage/controllers/getStorage.js
+++ src/apis/storage/controllers/getStorage.js
@@ -3,24 +3,24 @@ const ErrorModel = require("../../../models/api-error");
 
 const getStorage = async (req, res) => {
   try {
-    const { state,id } = req.query;
+    const { state, id } = req.query;
     if (id) {
       const storage = await StorageSchema.findOne({ id });
       if (!storage)
         return new ErrorModel().newNotFound("El deposito no existe").send(res);
       return res.status(200).send(storage);
-    }else if (state){
+    } else if (state) {
       const storages = await StorageSchema.find({ state });
       return res.status(200).send(storages);
-    } 
-    else {
+    } else {
       const storages = await StorageSchema.find();
-      const cont
-      for (let i  = 0; i < storages.length; i++) {
-        if(storages[i].state === "Activo"){
-          cont ++;
-        }
+      var cont = 0;
+      for (let i = 0; i < storages.length; i++) {
+        if (storages[i].area.available) cont++;
+        var total = cont / storages.length;
       }
+      console.log(total);
+      
       return res.status(200).send(storages);
     }
   } catch (err) {
