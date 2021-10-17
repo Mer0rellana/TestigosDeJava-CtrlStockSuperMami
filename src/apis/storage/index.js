bfd6e8906c9476e22a36f15c06c26f7b34d6b425
agustinagarciarey <agus.garciarey@gmail.com>
2021-10-17 13:50:40 -0300

    only get storage

--- src/apis/storage/index.js
+++ src/apis/storage/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const { ensureUserAuthenticated } = require('../../utils/token');
 const getStorage = require('./controllers/getStorage');
-const postStorage = require('./controllers/postStorage')
 const router = express.Router();
 
-
 router.get('/',getStorage);
-router.post('/add', ensureUserAuthenticated ,postStorage);
 
 module.exports = router;
