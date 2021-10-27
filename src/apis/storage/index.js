8b70cd0490811cee530fc0e890b9230cb6f08576
1w1111789AimonettiJoaquin <Yaimonetti@hotmail.com>
2021-10-27 12:43:41 -0300

    put storage

--- src/apis/storage/index.js
+++ src/apis/storage/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const { ensureUserAuthenticated } = require('../../utils/token');
 const getStorage = require('./controllers/getStorage');
 const postStorage = require('./controllers/postStorage');
-const putStorage = require('./controllers/putStorage')
+const putStorageState = require('./controllers/putStorageState')
 
 const router = express.Router();
 
 router.get('/',getStorage);
-router.put("/delete/:id",ensureUserAuthenticated, putStorage);
+router.put("/delete/:id",ensureUserAuthenticated, putStorageState);
 router.post("/add", ensureUserAuthenticated, postStorage)
 
 module.exports = router;
