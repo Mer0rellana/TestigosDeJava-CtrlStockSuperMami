70262ffdcb4c6c305b75f34da471dfef5d95b7a4
agustinagarciarey <agus.garciarey@gmail.com>
2021-11-01 22:55:28 -0300

    get inventory and get inventories

--- src/apis/stock/index.js
+++ src/apis/stock/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const { ensureUserAuthenticated } = require('../../utils/token');
 const PostStock = require('./controllers/postStock');
-
-
-const router = express.Router();
-
-
-router.post('/add', ensureUserAuthenticated, PostStock);
-const {ensureUserAuthenticated} = require('../../utils/token');
 const getStock = require('./controllers/getStock')
+
 const router =  express.Router();
 
 router.get('/',ensureUserAuthenticated,getStock);
+router.post('/add', ensureUserAuthenticated, PostStock);
 
 module.exports = router;
