03cb8721a6eaf52a8340a40f83e809135c2cfaaf
Mercedes <mer.p.orellana@gmail.com>
2021-11-11 23:36:11 -0300

    adjustment

--- src/views/components/inventario/ajuste/consultar-ajuste.js
+++ src/views/components/inventario/ajuste/consultar-ajuste.js
@@ -22,7 +22,7 @@ function crearTablaAjuste(datos) {
         html += "<td>" + datos[i].description + "</td>";
 
     html += `<td class="text-center"> 
-      <button class="edit" onclick="rellenarCampos(${datos[i]._id})" style="font-weight: 200; color: #ffbb2f; border: none; outline: none !important; -webkit-appearance: none !important;"
+      <button class="edit" onclick="rellenarCampoAjuste('${datos[i]._id}')" style="font-weight: 200; color: #ffbb2f; border: none; outline: none !important; -webkit-appearance: none !important;"
       data-toggle="modal" data-target="#modalInfoAjustes"  type="button"><i class="fas fa-info"></i></button>
     </td>`
     html += "</tr>"
@@ -31,9 +31,10 @@ function crearTablaAjuste(datos) {
 }
 ConsultarAjuste()
 
-function rellenarCampos(id){
+function rellenarCampoAjuste(id){
+    console.log(id);
     axios({
-        url: 'http://localhost:3000/adjustment/?id=' + id,
+        url: 'http://localhost:3000/adjustment/?_id=' + id,
         method: 'get',
         headers: {Authorization: `Bearer ${obj.token}`}
 
@@ -44,12 +45,12 @@ function rellenarCampos(id){
         document.getElementById('modalUsuario').value = data.data[0].idUser;
         document.getElementById('modalDescripcion').value = data.data[0].description;
         document.getElementById('modalStockSistema').value = data.data[0].systemStock;
-        document.getElementById('modalStockReal').value = data.data[0].realStock;
+        document.getElementById('modalRealStock').value = data.data[0].realStock;
         document.getElementById('modalStockFalladoSistema').value = data.data[0].failedSystemStock;
-        document.getElementById('modalStockRealFallado').value = data.data[0].failedRealStock;
+        document.getElementById('modalStockFalladoReal').value = data.data[0].failedRealStock;
         
-        console.log(data)
+        console.log(data.data)
         
-        console.log(data.data._id)
+        console.log(data.data[0].realStock)
     })
 }
